8a352687f86e1a69e42b06fbfc840a02fed513c5
Mikhail Titov <mlt@gmx.us>
2020-04-07 15:26:35 -0500

    Initial import for graphviz.portable package

---
+++ graphviz.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  PackageName   = 'graphviz.portable'
+  Url           = 'https://graphviz.gitlab.io/_pages/Download/windows/graphviz-2.38.zip'
+  Checksum      = '8e0eeb7aa6a84fc9c53d29ca9548a66af7762a03'
+  ChecksumType  = 'sha1'
+  UnzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ graphviz.portable/update.ps1
@@ -0,0 +1,30 @@
+﻿import-module au
+$NoCheckChocoVersion=1
+$root   = 'https://graphviz.gitlab.io/_pages/Download'
+$releases = "$root/Download_windows.html"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*Url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*Checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $re = 'graphviz-(.+)\.zip'
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $a = $url | Select-String -Pattern $re
+  $version = $a.matches.groups[1]
+
+  @{
+    URL32 = "$root/$url"
+    Version = $version
+    ChecksumType32 = 'sha1'
+  }
+}
+
+update -ChecksumFor 32 -NoCheckChocoVersion
