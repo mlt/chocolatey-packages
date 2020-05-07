ee0ff2de36f34d274b045f050ecb511692ac1da9
Simran <Simran-B@users.noreply.github.com>
2020-05-07 19:34:35 +0200

    Update doxygen to v1.8.18 (#3)

--- doxygen.install/tools/chocolateyInstall.ps1
+++ doxygen.install/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
   PackageName  = 'doxygen.install'
   FileType     = 'exe'
   SilentArgs   = '/VERYSILENT'
-  Url          = 'https://sourceforge.net/projects/doxygen/files/rel-1.8.17/doxygen-1.8.17-setup.exe/download'
-  Checksum     = '1ef86daf5d41f56fa62d827027dda291968cd1dc'
+  Url          = 'https://sourceforge.net/projects/doxygen/files/rel-1.8.18/doxygen-1.8.18-setup.exe/download'
+  Checksum     = 'c781783f594eadaf7f3c2983dc7f9a1c99e91c90'
   ChecksumType = 'sha1'
 }
 
