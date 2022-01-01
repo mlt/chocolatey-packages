6217ac0daedf725a4754928f4d6c3b920cfb986e
Chocolatey <chocolatey@realdimensions.net>
2022-01-01 12:35:24 +0000

    AU: 1 updated - doxygen.install [skip ci] https://gist.github.com/4164c68d7b8bce26f76d7f306d223d2f/0eca2467c17e69c1b7bf4e6d0bd68be5856efc0d

--- doxygen.install/tools/chocolateyInstall.ps1
+++ doxygen.install/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
   PackageName  = 'doxygen.install'
   FileType     = 'exe'
   SilentArgs   = '/VERYSILENT'
-  Url          = 'https://sourceforge.net/projects/doxygen/files/rel-1.9.2/doxygen-1.9.2-setup.exe/download'
-  Checksum     = '16103418cd99629971b2960394dc4493af612afc'
+  Url          = 'https://sourceforge.net/projects/doxygen/files/rel-1.9.3/doxygen-1.9.3-setup.exe/download'
+  Checksum     = '24fd29b4476ad1197a41438efcd67b926db923cd'
   ChecksumType = 'sha1'
 }
 
